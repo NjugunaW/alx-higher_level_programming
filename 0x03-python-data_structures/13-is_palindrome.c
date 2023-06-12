f0569cb02fc00c9dcfb8daa1beb6805f51af805f
NjugunaW <njugunawangui10@gmail.com>
2023-06-12 07:03:59 -0700

    Completed Project

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,68 +1,40 @@
 #include "lists.h"
-#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * list_reverse - a function that reverses a singly-linked list
- * @head: pointer to a struct
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *list_reverse(listint_t **head)
-{
-	listint_t *nde = *head, *next, *last = NULL;
-
-	while (nde)
-	{
-		next = nde->next;
-		nde->next = last;
-		last = nde;
-		nde = next;
-	}
-
-	*head = last;
-	return (*head);
-}
-
-/**
- * is_palindrome - a function that checks if a list is a palindrome
- * @head: pointer to a sturct
- * Return: 0 if linked list not a palindrome
- *         1 if linked list is a palindrome
- */
+  * is_palindrome - fnctn that checks if a singly linked list is a palindrome
+  * @head: pointer to a struct
+  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp, *rev, *centre;
-	size_t size = 0, i;
+	listint_t *tmp = *head;
+	int nde = 0, x = 0, *arr = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL || head == NULL || (*head)->next == NULL)
 		return (1);
-
-	temp = *head;
-	while (temp)
+	while (tmp)
 	{
-		size++;
-		temp = temp->next;
+		nde++;
+		tmp = tmp->next;
 	}
-
-	temp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		temp = temp->next;
-
-	if ((size % 2) == 0 && temp->n != temp->next->n)
-		return (0);
-
-	temp = temp->next->next;
-	rev = list_reverse(&temp);
-	centre = rev;
-
-	temp = *head;
-	while (rev)
+	arr = malloc(sizeof(int) * nde);
+	tmp = *head;
+	while (tmp)
 	{
-		if (temp->n != rev->n)
+		arr[x++] = tmp->n;
+		tmp = tmp->next;
+	}
+	for (x = 0; x < nde / 2; x++)
+	{
+		if (arr[x] != arr[nde - 1 - x])
+		{
+			free(arr);
 			return (0);
-		temp = temp->next;
-		rev = rev->next;
+		}
 	}
-	list_reverse(&centre);
-
+	free(arr);
 	return (1);
 }
