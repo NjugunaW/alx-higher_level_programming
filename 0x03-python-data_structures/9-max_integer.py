d04f9d351a3d4f707f0c1a773097d15dba9a4c07
NjugunaW <njugunawangui10@gmail.com>
2023-06-12 05:55:42 -0700

    Completed Project

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return (None)
+
     biggest = my_list[0]
     for num in range(len(my_list)):
         if my_list[num] > biggest:
