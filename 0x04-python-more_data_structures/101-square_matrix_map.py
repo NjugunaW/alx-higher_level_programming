d14c8965a8fd9996b61de9cf10b282843a84c757
NjugunaW <njugunawangui10@gmail.com>
2023-06-14 11:56:36 -0700

    Completed Project

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda w: list(map(lambda a: a**2, w)), matrix)))
+    return list(map(lambda w: list(map(lambda n: n ** 2, w)), matrix))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
 
+def roman_to_int(roman_string):
+   
     if roman_string is None or type(roman_string) is not str:
         return (0)
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
@@ -14,7 +15,7 @@ def roman_to_int(roman_string):
             result += roman_numerals[letter]
             if roman_numerals[letter] > prev_value:
                 result -= prev_value * 2
-             prev_value = roman_numerals[letter]
-         else:
-             return (0)
-    return(result)
+            prev_value = roman_numerals[letter]
+        else:
+            return (0)
+    return (result)
