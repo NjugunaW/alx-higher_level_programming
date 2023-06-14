7cc628ae1de1e0f34e2ce48cbf5a2701e10e541b
NjugunaW <njugunawangui10@gmail.com>
2023-06-14 08:06:55 -0700

    Completed Project

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))
+    [print("{}: {}".format(key, a_dictionary[key]))
+            for key in sorted(a_dictionary.keys()):]
 
