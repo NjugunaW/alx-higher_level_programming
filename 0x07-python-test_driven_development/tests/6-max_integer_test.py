e6de2604382eeaabdd72026caf018c2ec6cc33e2
NjugunaW <njugunawangui10@gmail.com>
2023-07-05 06:07:28 -0700

    Corrections

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
- """
- Class that tests a max int function
- """
+    """
+    A class to test a max integer function
+    """
 
     def test_max_integer(self):
         """
-        This tests tha max integer in a list with positive and negative numbers
+        Test the max integer in a list of integers when the integers
+        are positive or negative numbers
         """
-        self.assertEqual(max_integer([2, 7, 9, 6]), 9)
-        self.assertEqual(max_integer([-2, -7, -9, -6]), -2)
-        self.assertEqual(max_integer([2.2, 7.3, 9.7, 6.9]), 9.7)
-        self.assertEqual(max_integer([5, 0]), 5)
         self.assertIsNone(max_integer([]))
-        self.assertEqual(max_integer([20000000, 100000000]), 100000000)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertAlmostEqual(max_integer([-4, -3, -2, -1, 0]), 0)
+        self.assertAlmostEqual(max_integer([-90, -120, -150, -180]), -90)
+        self.assertAlmostEqual(max_integer([1.0, 1.5, 1.6, 3.7, 2.3]), 3.7)
+        self.assertAlmostEqual(max_integer([7.7]), 7.7)
 
     def test_wrong_types(self):
         """
-        Tests the max int in a list with wrong parameter types
+        Test the max integer with wrong parameters types
         """
         with self.assertRaises(TypeError):
             max_integer(None)
 
         with self.assertRaises(TypeError):
-            max_integer(["Wow", 2, "Hello" 3, "There"])
+            max_integer(["Monty", 89, 34, -9.7, "Python"])
