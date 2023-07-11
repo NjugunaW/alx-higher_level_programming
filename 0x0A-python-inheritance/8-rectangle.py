e4e8e782601ec24e13050d703eb2cc6054e8876b
NjugunaW <njugunawangui10@gmail.com>
2023-07-10 19:31:30 -0700

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 
+"""
+This module contains an empty class
+"""
+
 BaseGeometry = __import__ ('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """
-    A class that inherits form Basegeometry
+    A class Rectangle that inherits from  Basegeometry
     """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
 
+
+    def __init__(self, width, height):
+        """
+        This method is called when a new object is created
+        Args:
+        :width:
+        :height:
+        """
+
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
