dd2bdc21aff94ac844a73c1b03ed90b6f815b5f6
NjugunaW <njugunawangui10@gmail.com>
2023-07-11 18:38:20 -0700

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module appends a string at the end of a text file
+This module appends a string 
+at the end of a text file
 """
 
 def append_write(filename="", text=""):
@@ -8,12 +9,9 @@ def append_write(filename="", text=""):
     This module appends a string at the end of a text file (UTF8) and
     returns the number of characters added
 
-    Args:
-        filename (flds): Name of the file
-        text (string): String to append
-
-    Returns: 
-        returns the number of characters added
+    :filename:
+    :text:
+    :return: Number of characters added
     """
-    with open(filename, 'a', encoding='utf-8') as f:
-        return f.write(text)
+    with open(filename, 'a', encoding='utf') as f_ile:
+        return f_ile.write(text)
