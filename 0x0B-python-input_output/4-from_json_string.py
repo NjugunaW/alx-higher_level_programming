2c088dfcaaa4f8250766127cb641b90c8e616344
NjugunaW <njugunawangui10@gmail.com>
2023-07-11 18:52:51 -0700

    Task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+
 """
-A module that returns an object (Python data structure)
+A module that returns an object
 represented by a JSON string
 """
+
 import json
+
 
 def from_json_string(my_str):
     """
-    This module returns an object (Python data structure) represented by a JSON string
+    This module returns an object represented by a JSON string
+    :my_str:
+    :return:
     """
-
     return json.loads(my_str)
