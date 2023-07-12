81e99d29de51edc0be7d0f0e1ce728363dae0fea
NjugunaW <njugunawangui10@gmail.com>
2023-07-11 19:21:22 -0700

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
+
+
 """
-A class Student that defines jr student
+This module has a class Student that defines a Student
 """
 
 
 class Student:
     """
-    Class Student
+    The Class Student
     """
+
     def __init__(self, first_name, last_name, age):
         """
-        This method is called to initalize variable instances
-        Args:
-            first_name (str): First name
-            last_name (str): Last name
-            age (int): Age
+        This function is called to initalize variable instances
+        :first_name:
+        :last_name:
+        :age:
         """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrbts=None):
+    def to_json(self, attrs=None):
         """
-        This function retrieves a dictionary representation of a Student instance
+        This function retrieves a dictionary representation
+        of a Student instance
+        :attrs:
+        :return:
         """
-        if attrbts is None:
+        if attrs is None:
             return self.__dict__
         new_dict = {}
-        for jr in attrbts:
+        for jr in attrs:
             try:
                 new_dict[jr] = self.__dict__[jr]
             except FileNotFoundError:
@@ -36,7 +40,11 @@ class Student:
         return new_dict
 
     def reload_from_json(self, json):
-        """This function replaces all attributes of the Student instance"""
+        """
+        This function replaces all attributes of the Student instance
+        :json:
+        :return:
+        """
         for key in json:
             try:
                 setattr(self, key, json[key])
