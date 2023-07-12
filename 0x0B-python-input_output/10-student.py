984df9d91f914191bb9414d8bb8e5e71b5440aa6
NjugunaW <njugunawangui10@gmail.com>
2023-07-11 19:17:41 -0700

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
+
+
 """
-A Student that defines a student
+This module has a class Student
 """
 
 
 class Student:
     """
-    A class Student that defines a student.
+    The Class Student that defines a student
     """
 
     def __init__(self, first_name, last_name, age):
-        """This method is called to initialize variable  instances
-
-        Attributes:
-                  first_name (str): The first name of the student.
-                  last_name (str): The last name of the student.
-                  age (int): The age of the student.
+        """
+        This function instantiates the class variables
+        :first_name:
+        :last_name:
+        :age:
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrbts=None):
+    def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of the Student instance.
-        
-        Attributes:
-                  attrbts (list): Attributes
+        This function retrieves a dictionary representation
+        of a Student instance
+        :param attrs:
+        :return:
         """
-
-        if (type(attrbts) == list and
-                all(type(ab) == str for ab in attrbts)):
-            return {k: getattr(self, k) for k in attrbts if hasattr(self, k)}
+        if type(attrs) == list and all(type(ab) == str for ab in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
