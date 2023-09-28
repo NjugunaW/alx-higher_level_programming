3e9709eec9cd3c87b0a093c24d465f758dedb55b
NjugunaW <njugunawangui10@gmail.com>
2023-09-28 12:12:21 -0700

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""A function that finds a peak in a list of unsorted integers"""
+"""
+A function that finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
     """
-    A function that finds a peak
-    in a list of unsorted integers.
+    A function that finds a peak in a list of unsorted integers.
     """
     if list_of_integers == []:
         return None
-    _len_ = len(list_of_integers)
-    x = int(_len_ / 2)
-    abt = list_of_integers
 
-    if x - 1 < 0 and x + 1 >= _len_:
-        return abt[x]
-    elif x - 1 < 0:
-        return abt[x] if abt[x] > abt[x + 1] else abt[x + 1] \
-            else abt[x + 1]
-    elif x + 1 >= _len_:
-        return abt[x] if abt[x] > abt[x - 1] \
-            else abt[x - 1]
+    _length_ = len(list_of_integers)
+    avrg = int(_length_ / 2)
+    frsh_lst = list_of_integers
 
-    if abt[x - 1] < abt[x] > abt[x + 1]:
-        return abt[x]
+    if avrg - 1 < 0 and avrg + 1 >= _length_:
+        return frsh_lst[avrg]
+    elif avrg - 1 < 0:
+        return frsh_lst[avrg] if frsh_lst[avrg] > frsh_lst[avrg + 1] \
+            else frsh_lst[avrg + 1]
+    elif avrg + 1 >= _length_:
+        return frsh_lst[avrg] if frsh_lst[avrg] > frsh_lst[avrg - 1] \
+            else frsh_lst[avrg - 1]
 
-    if abt[x + 1] > abt[x - 1]:
-        return find_peak(abt[x:])
-    return find_peak(abt[:x])
+    if frsh_lst[avrg - 1] < frsh_lst[avrg] > frsh_lst[avrg + 1]:
+        return frsh_lst[avrg]
+
+    if frsh_lst[avrg + 1] > frsh_lst[avrg - 1]:
+        return find_peak(frsh_lst[avrg:])
+    return find_peak(frsh_lst[:avrg])
