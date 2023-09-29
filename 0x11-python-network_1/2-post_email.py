6f40e920a3ae5b33bd1fb2cf167590ddf90ddd62
NjugunaW <njugunawangui10@gmail.com>
2023-09-29 14:45:20 -0700

    Task 1 & 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL and an email, sends a POST request to the passed URL
+""" Python script that takes in a URL and an email,
+sends a POST request to the passed URL
 """
 
 
